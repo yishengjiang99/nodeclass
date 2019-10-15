1019710fad6189e3b7a4d7eae8bc4f7463530d78
Yisheng Jiang <yisheng.jiang@gmail.com>
2019-10-15 12:17:29 -0700

    node mongoddb exmaple 2

---
+++ node-mongo/index.js
@@ -0,0 +1,44 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+const url = 'mongodb://localhost:27017/';
+const dbname = 'conFusion';
+
+
+const dboper = require('./operations');
+
+MongoClient.connect(url, (err, client) => {
+
+    assert.equal(err,null);
+
+    console.log('Connected correctly to server');
+
+    const db = client.db(dbname);
+    const collection = db.collection("dishes");
+
+dboper.insertDocument(db, { name: "Vadonut", description: "Test"},
+        "dishes", (result) => {
+            console.log("Insert Document:\n", result.ops);
+
+            dboper.findDocuments(db, "dishes", (docs) => {
+                console.log("Found Documents:\n", docs);
+
+                dboper.updateDocument(db, { name: "Vadonut" },
+                    { description: "Updated Test" }, "dishes",
+                    (result) => {
+                        console.log("Updated Document:\n", result.result);
+
+                        dboper.findDocuments(db, "dishes", (docs) => {
+                            console.log("Found Updated Documents:\n", docs);
+
+                            db.dropCollection("dishes", (result) => {
+                                console.log("Dropped Collection: ", result);
+
+                                client.close();
+                            });
+                        });
+                    });
+            });
+    });
+
+});
