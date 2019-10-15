d4b39d35ecd0bb71583c0911f0fc8a37f139b83b
Yisheng Jiang <yisheng.jiang@gmail.com>
2019-10-15 15:52:12 -0700

    mongos 3

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -1,32 +1,40 @@
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+const mongoose = require("mongoose");
+const url = 'mongodb://localhost:27017/conFusion';
+const Dishes = require("./models/dishes");
+const connect = mongoose.connect(url);
 
-const url = 'mongodb://localhost:27017/';
-const dbname = 'conFusion';
 
-
-const dboper = require('./operations');
-
-MongoClient.connect(url).then((client)=>{
+connect.then((client) => {
     console.log('Connected correctly to server');
-    const db = client.db(dbname);
-
-    dboper.insertDocument(db, { name: "Vadonut", description: "Test"},
-        "dishes")
-        .then((result)=>{
-            console.log("Insert Document:\n", result.ops);
-            return dboper.updateDocument(db,
-                {name:"Vadonut"},
-                {description:"updated descipt"},
-                "dishes")
-        }).then((result)=>{
-            console.log("Updated Document:\n", result.result);
-            return dboper.findDocuments(db, "dishes")
-        }).then((docs)=>{
-            console.log("Found Updated Documents:\n", docs);
-            return db.dropCollection("dishes");
-        }).then((result)=>{
-            console.log("Dropped Collection: ", result);
-            client.close();
+    // const db = client.db(dbname);
+    var newDish = Dishes({
+        name:"pizza444",
+        description:"test"
+    });
+    newDish.save()
+    .then((dish)=>{
+        dish.name='yisheng2';
+        dish.save();
+        
+        console.log(JSON.stringify(dish));
+        return Dishes.findByIdAndUpdate(dish._id,{
+            description:"yisheng dish"
+        },{
+            new: true
+        }).exec();
+    }).then((dish)=>{
+        dish.comments.push({
+            rating:5,
+            comment:"great",
+            author:"yisheng"
         })
-}).catch(console.log);;
+        return dish.save();
+    }).then((dish)=>{
+        console.log(JSON.stringify(dish));
+        return mongoose.connection.close();
+    }).catch((err)=>{
+        console.log(err);
+    }).finally(()=>{
+        mongoose.connection.close();
+    })
+}).catch(console.log)
